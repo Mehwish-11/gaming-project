97b2cd916c76b098ab4e0643fa3a8590d746a64a
Mehwish <Womenintech.mail@gmail.com>
2025-08-29 15:34:24 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,47 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <main>
+    <main className='realative w-full h-screen overflow-hidden flex justify-center mb-[10%]'>
+        <video src="public\videos\hero.mp4"
+            //autoPlay
+            //loop
+            muted
+            playsInline
+            className='w-full h-[95%] object-cover absolute top-0 first-letter:left-0 -z-10'>
+        </video>
 
+        <div className='absolute bottom-[15%] flex flex-col gap-5 items-center'>
+            <img src='public\images\illu-text.png' alt='Illu-text'
+            className='md:w-[30rem] w-[20rem]' 
+            />
+            <h1 className='md:text-2xl text-1xl
+            font-bold'>
+                Explore, Capture, Conqure
+            </h1>
+            <div className='md:w-[70%] w-[60%] h[0.1px]
+            bg-[#baba]'> </div>
+
+            <button className="h-10 px-16 bg-gradient-to-r from-purple-500 to-indigo-600 
+                rounded-lg font-medium text-nowrap hover:opacity-70 transition-all duration-300">
+                    PLAY NOW 
+            </button>
+            <div className='flex items-center gap-5 text-3xl font-extrabold text-gray-200'>
+                <img className='md:h-16 h-12'
+                src='public\images\illu-logo.png' alt='illu-logo'/>
+                ZERO
+            </div>
+            <p className='max-w-[80%] text-center text-[#babaff]'>
+                Notice; Illucium Games are in Bets. Participation involves risk.
+                Read our full Disclaimer here.
+            </p>
+        </div>
+        <div className='absolute bottom-40 lg:right-24 right-5 mt-24 animate-bounce sm:inline-block hidden'>
+            <div>
+                <div className='w-8 h-12 border-2 border-[#babaff] rounded-full flex justify-center pt-1'>
+                    <div className='w-1 h-3 bg-[#babaff] rounded-full animate-pulse'></div>
+                </div>
+            </div>
+        </div>
     </main>
   )
 }
