b67b2bac3e95319a16dff97fe06c5f89b5513782
Mehwish <Womenintech.mail@gmail.com>
2025-08-29 14:11:41 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,25 @@
 
 const Header = () => {
   return (
-    <header>
+    <header className="py-1 px-7 flex justify-between items-center fixed top-0 z-50 w-full border-b-[0.3px]
+        border--[#babaff]">
         {/*left section*/}
-        <div>
+        <div className="flex lg:gap-14 gap-4 items-center">
             <img className="md:w-16 w-12" src="public\images\logo.png" alt="logo-img" />
 
-            <div>
+            <div className="hidden md:flex gap-5 items-center">
                 <button className="h-8 px-6 bg-gradient-to-r from-purple-500 to-indigo-600 
                 rounded-lg font-medium text-nowrap hover:opacity-70 transition-all duration-300">
                     PLAY NOW 
                 </button>
 
-                <button className="h-8 px-6 bg-gradient-to-r from-purple-500 to-indigo-600 
+                <button className="h-8 px-6 bg-gradient-to-r from-gray-600 to-gray-400 
                 rounded-lg font-medium text-nowrap hover:opacity-70 transition-all duration-300">
                     PLAY NOW 
                 </button>
             </div>
         </div>
+        
     </header>
   )
 }
