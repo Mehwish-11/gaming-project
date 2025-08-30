90f8e2c4947da782490b8f2737e88d44ae1cebee
Mehwish <Womenintech.mail@gmail.com>
2025-08-30 19:02:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Charecters from "./components/Charecters"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Arena from "./components/Arena"
 const App = () => {
   return (
     <div className=''>
       <Header/>
       <Hero/>
       <Charecters/>
+      <Arena/>
     </div>
   )
 }
