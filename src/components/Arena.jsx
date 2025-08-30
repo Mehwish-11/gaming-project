d80e66183e81733c3e768edf4e3a4751cfa88fec
Mehwish <Womenintech.mail@gmail.com>
2025-08-30 19:19:44 +0530

    Update Arena.jsx

--- src/components/Arena.jsx
+++ src/components/Arena.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Arena = () => {
   return (
-  <div>
-    
+  <div className='md:min-h-[90%] p-0 lg:p-8 md:mt-0 mt-60 '>
+    <div className='relative z-0 pt-6 text-center'>
+        <h1 className='text-5xl font-bold tracking-widest mb-28'
+        style={{textShadow: "0 0 10px rgba(255, 255, 255, 0.7), 0 0 20px rgba(255, 255, 255, 0.5)"}}>
+            Arena 
+        </h1>
+    </div>
+    {/* Arena container with grid layout*/}
+    <div className='lg:max-w-[90%] mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6'>
+
+        {/* left column */}
+
+    </div>
+
   </div>
   )
 }
