deb4cdd01df1a034bf5e62500e87878c3962a216
Mehwish <Womenintech.mail@gmail.com>
2025-08-30 17:59:24 +0530

    Update Charecters.jsx

--- src/components/Charecters.jsx
+++ src/components/Charecters.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import { Star } from 'lucide-react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Charecters = () => {
   //track which avatar is selected
-  const [selectedAvtar, setSelectedAvtar] = useState("VIKI"); 
+  const [selectedAvtar, setSelectedAvtar] = useState("VIKI");
 
   //simplified avatar data
   const Avatar = {
@@ -12,15 +14,15 @@ const Charecters = () => {
       stable: 95,
       penetrate: 30,
       portable: 80,
-      stars:3,
+      stars: 3,
     },
-    EVA : {
+    EVA: {
       name: "EVA",
       power: 90,
       stable: 80,
       penetrate: 70,
       portable: 60,
-      stars:4,
+      stars: 4,
     },
   }
 
@@ -50,7 +52,7 @@ const Charecters = () => {
           {/**selected awatar info card */}
           <div className='bg-gray-900/80 backdrop-blur-sm rounded-lg p-4 mb-4 border border-gray-800
             shadow-[0_0_15px_rgba(167, 139, 250,0.2)]'>
-              <h1 className='text-2xl font-semibold mb-2'>{currentAvtar.name}</h1>
+            <h1 className='text-2xl font-semibold mb-2'>{currentAvtar.name}</h1>
 
             {/**avtar statistics */}
 
@@ -63,7 +65,7 @@ const Charecters = () => {
                 <div className='flex-1 h-4 bg-gray-800 rounded-full overflow-hidden'>
                   <div className='h-full bg-gradient-to-r from-violet-600 to-white'
                     style={{ width: `${currentAvtar.power}%` }}>
-                     
+
                   </div>
                 </div>
                 <span className='ml-2'>{currentAvtar.power}</span>
@@ -75,7 +77,7 @@ const Charecters = () => {
                 <span className='w-24 text-gray-400'> Stable </span>
                 <div className='flex-1 h-4 bg-gray-800 rounded-full overflow-hidden'>
                   <div className='h-full bg-gradient-to-r from-violet-600 to-white'
-                   style={{ width: `${currentAvtar.stable}%` }}>
+                    style={{ width: `${currentAvtar.stable}%` }}>
 
                   </div>
                 </div>
@@ -88,7 +90,7 @@ const Charecters = () => {
                 <span className='w-24 text-gray-400'> Penetrate </span>
                 <div className='flex-1 h-4 bg-gray-800 rounded-full overflow-hidden'>
                   <div className='h-full bg-gradient-to-r from-violet-600 to-white'
-                   style={{ width: `${currentAvtar.penetrate}%` }}>
+                    style={{ width: `${currentAvtar.penetrate}%` }}>
 
                   </div>
                 </div>
@@ -101,31 +103,119 @@ const Charecters = () => {
                 <span className='w-24 text-gray-400'> Portable </span>
                 <div className='flex-1 h-4 bg-gray-800 rounded-full overflow-hidden'>
                   <div className='h-full bg-gradient-to-r from-violet-600 to-white'
-                   style={{ width: `${currentAvtar.portable}%` }}>
+                    style={{ width: `${currentAvtar.portable}%` }}>
 
                   </div>
                 </div>
                 <span className='ml-2'>{currentAvtar.portable}</span>
               </div>
-              </div>
+            </div>
 
-              {/**Action button */}
-              <div className='flex gap-3'>
-                <button className='px-4 py-1 bg-violet-900 text-white rounded-md font-semibold
+            {/**Action button */}
+            <div className='flex gap-3'>
+              <button className='px-4 py-1 bg-violet-900 text-white rounded-md font-semibold
                 hover:opacity-70 transition-all duration-300'>
-                  Profitient
-                </button>
+                Profitient
+              </button>
 
-                <button className='px-4 py-1 bg-violet-900 text-white rounded-md font-semibold
+              <button className='px-4 py-1 bg-violet-900 text-white rounded-md font-semibold
                 hover:opacity-70 transition-all duration-300'>
-                  Redemption
-                </button>
+                Redemption
+              </button>
             </div>
           </div>
 
           {/**Avatar selection Cards */}
+          <div className='grid grid-cols-2 gap-4'>
+            {/** Vicky card */}
+            <div className='relative bg-gray-900/70 
+             backdrop-blur-sm rounded-lg p-3 border
+             flex lg:flex-row flex-col justify-between
+             px-12 items-center cursor-pointer transition-all
+             duration-300'
+             onClick={() => setSelectedAvtar("VIKI")}>
+              <div className='text-lg mb-2'>
+                VIKI
+              </div>
+              {/** Avatar visual placeholder */}
+              <div className='w-20 h-20 bg-gray-800/50
+               rounded-md flex items-center justify-center
+               mb-2'>
+                <img src='public\images\VIKI.png' alt='VIKI-IMG' />
+              </div>
+              {/* star rating */}
+              <div className='flex '>
+                {[...Array(3)].map((_, i) => (
+                  <Star key={i} className='w-4 h-4 fill-violet-400
+                   text-violet-500'/>
+                ))}
+              </div>
 
-          
+              {selectedAvtar === "VIKI" && (
+                <div className='absolute inset-0 border-2 rounded-lg pointer-events-none'>
+                </div>
+              )}
+            </div>
+
+            {/** EVA card */}
+            <div className='relative bg-gray-900/70 
+             backdrop-blur-sm rounded-lg p-3 border
+             flex lg:flex-row flex-col justify-between
+             px-12 items-center cursor-pointer transition-all
+             duration-300'
+             onClick={() => setSelectedAvtar("EVA")}>
+              <div className='text-lg mb-2'>
+                EVA
+              </div>
+              {/** Avatar visual placeholder */}
+              <div className='w-20 h-20 bg-gray-800/50
+               rounded-md flex items-center justify-center
+               mb-2'>
+                <img src='public\images\EVA.png' alt='EVA-IMG' />
+              </div>
+              {/* star rating */}
+              <div className='flex '>
+                {[...Array(4)].map((_, i) => (
+                  <Star key={i} className='w-4 h-4 fill-violet-400
+                   text-violet-500'/>
+                ))}
+              </div>
+
+              {selectedAvtar === "EVA" && (
+                <div className='absolute inset-0 border-2 rounded-lg pointer-events-none'>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        {/* right side - Avtar visual */}
+        <div className='relative md:w-2/4 w-full md:h-full h-80 flex items-center
+         justify-center overflow-hidden'>
+          <AnimatePresence mode='wait'>
+          {selectedAvtar === "VIKI" ? (
+            <motion.div
+              key="VIKI"
+              className='absolute inset-0'
+              initial={{x:"100%"}}
+              animate={{x:0}}
+              exit={{x:"-100%"}}
+              transition={{duration:0.5}}
+              >
+              <img src='public\images\VIKI.png' alt=''/>
+            </motion.div>
+          ) : (
+            <motion.div
+              key="EVA"
+              className='absolute inset-0'
+              initial={{x:"100%"}}
+              animate={{x:0}}
+              exit={{x:"-100%"}}
+              transition={{duration:0.5}}
+              >
+              <img src='public\images\EVA.png' alt=''/>
+            </motion.div>
+          )}
+          </AnimatePresence>
         </div>
       </div>
     </div>
