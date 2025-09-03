0b42d85f2682fd69b909565f7cb9c71a066f7cb5
Mehwish <Womenintech.mail@gmail.com>
2025-09-04 00:44:43 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         border--[#babaff] bg-black ">
         {/*left section*/}
         <div className="flex lg:gap-14 gap-4 items-center">
-            <img className="md:w-16 w-12" src="public\images\logo.png" alt="logo-img" />
+            <img className="md:w-16 w-12" src="/images/logo.png" alt="logo-img" />
 
             <div className="hidden md:flex gap-5 items-center">
                 <button className="h-8 px-6 bg-gradient-to-r from-purple-500 to-indigo-600 
